5743c0ed60e62a7a296c40d25cc4a5c555ad7357
Johannes Gorset <jgorset@gmail.com>
2011-08-08 15:01:10 +0200

    Bump version to 0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'respite',
-    version = '0.9.2',
+    version = '0.10',
     description = "Respite conforms Django to Representational State Transfer (REST)",
     author = "Johannes Gorset",
     author_email = "jgorset@gmail.com",
