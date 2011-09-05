e2856fb1b4323433a1703ab93cce3c08b2a0f958
Johannes Gorset <jgorset@gmail.com>
2011-09-05 20:20:11 +0200

    Implement XML serialization

--- respite/serializers/__init__.py
+++ respite/serializers/__init__.py
@@ -1,6 +1,8 @@
 from respite.serializers.jsonserializer import JSONSerializer
+from respite.serializers.xmlserializer import XMLSerializer
 from respite import formats
 
 serializers = {
-    formats.find('JavaScript Object Notation'): JSONSerializer
+    formats.find('JavaScript Object Notation'): JSONSerializer,
+    formats.find('Extensible Markup Language'): XMLSerializer
 }
---
+++ respite/serializers/xmlserializer.py
@@ -0,0 +1,71 @@
+from xml.etree import ElementTree as ET
+
+from respite.inflector import singularize
+from base import Serializer
+
+class XMLSerializer(Serializer):
+
+    def serialize(self):
+        data = self.preprocess()
+
+        root = ET.Element('response')
+
+        def serialize(key, value):
+
+            def serialize_boolean(boolean):
+                element = ET.Element(key)
+                element.text = 'true' if boolean else 'false'
+
+                return element
+
+            def serialize_string(string):
+                element = ET.Element(key)
+                element.text = string
+
+                return element
+
+            def serialize_list(list):
+                element = ET.Element(key)
+
+                for item in list:
+                    subelement = serialize(singularize(key), item)
+                    element.append(subelement)
+
+                return element
+
+            def serialize_dictionary(dictionary):
+                element = ET.Element(key)
+
+                for subelement_key, subelement_value in dictionary.items():
+                    subelements = serialize(subelement_key, subelement_value)
+                    element.append(subelements)
+
+                return element
+
+            def serialize_integer(integer):
+                element = ET.Element(key)
+                element.text = '%s' % integer
+
+                return element
+
+            if isinstance(value, bool):
+                return serialize_boolean(value)
+
+            if isinstance(value, basestring):
+                return serialize_string(value)
+
+            if isinstance(value, list):
+                return serialize_list(value)
+
+            if isinstance(value, dict):
+                return serialize_dictionary(value)
+
+            if isinstance(value, int):
+                return serialize_integer(value)
+
+            raise TypeError("Respite doesn't know how to serialize %s as XML" % value.__class__.__name__)
+
+        for key, value in data.items():
+            root.append(serialize(key, value))
+
+        return '<?xml version="1.0" encoding="UTF-8"?>' + ET.tostring(root)
