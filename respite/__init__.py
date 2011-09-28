85fb1d8f335502b2fe5c6cec822b014e844a4bbd
Johannes Gorset <jgorset@gmail.com>
2011-09-28 16:07:34 +0200

    Bump version to 0.11

--- respite/__init__.py
+++ respite/__init__.py
@@ -1,3 +1,3 @@
 from respite.views import *
 
-VERSION = '0.10.4'
+VERSION = '0.11.0'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'respite',
-    version = '0.10.4',
+    version = '0.11',
     description = "Respite conforms Django to Representational State Transfer (REST)",
     author = "Johannes Gorset",
     author_email = "jgorset@gmail.com",
