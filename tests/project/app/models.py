5e9eb03e1c780beb7fa4285c8eac56134501b68d
Johannes Gorset <jgorset@gmail.com>
2012-02-09 22:47:23 +0100

    Add test for Model#serialize

--- tests/project/app/models.py
+++ tests/project/app/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
