528f15c2632d91c8ede9437fb27b6a67d2b8471a
Espen Høgbakk <espen@hogbakk.no>
2012-07-23 17:47:06 +0200

    Add test for JsonMiddleware

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,47 @@
 """Test utilities."""
 
+from django.test import Client
+from django.core.handlers.wsgi import WSGIRequest
+
 def monkeypatch_method(cls):
     """Monkeypatch the given function to the given class."""
     def decorator(func):
         setattr(cls, func.__name__, func)
         return func
     return decorator
+
+class RequestFactory(Client):
+    """
+    Class that lets you create mock Request objects for use in testing.
+    
+    Usage:
+    
+    rf = RequestFactory()
+    get_request = rf.get('/hello/')
+    post_request = rf.post('/submit/', {'foo': 'bar'})
+    
+    This class re-uses the django.test.client.Client interface, docs here:
+    http://www.djangoproject.com/documentation/testing/#the-test-client
+    
+    Once you have a request object you can pass it to any view function, 
+    just as if that view had been hooked up using a URLconf.
+    
+    """
+    def request(self, **request):
+        """
+        Similar to parent class, but returns the request object as soon as it
+        has created it.
+        """
+        environ = {
+            'HTTP_COOKIE': self.cookies,
+            'PATH_INFO': '/',
+            'QUERY_STRING': '',
+            'REQUEST_METHOD': 'GET',
+            'SCRIPT_NAME': '',
+            'SERVER_NAME': 'testserver',
+            'SERVER_PORT': 80,
+            'SERVER_PROTOCOL': 'HTTP/1.1',
+        }
+        environ.update(self.defaults)
+        environ.update(request)
+        return WSGIRequest(environ)
