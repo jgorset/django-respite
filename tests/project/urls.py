ddac9095f2a8949726807ce8c458ad5e1b0fcadd
Johannes Gorset <jgorset@gmail.com>
2012-08-01 10:14:50 +0200

    Add tests for HttpMethodOverride

--- tests/project/urls.py
+++ tests/project/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
+from views import home
+
 urlpatterns = patterns('',
+    (r'^$', home),
     (r'^news/', include('tests.project.app.urls'))
 )
--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,9 +1,11 @@
 """Tests for respite.middleware."""
 
-from django.utils import simplejson as json
-from django.test.client import Client
+from nose.tools import *
 
-from respite.middleware import JsonMiddleware
+from django.utils import simplejson as json
+from django.test.client import Client, RequestFactory
+
+from respite.middleware import JsonMiddleware, HttpMethodOverrideMiddleware
 
 client = Client()
 
@@ -17,3 +19,16 @@ def test_json_middleware():
         }),
         content_type = 'application/json'
     )
+
+def test_http_method_override_middleware():
+    request = RequestFactory().post(
+        path = '/',
+        data = {
+            'foo': 'bar',
+            '_method': 'PUT'
+        }
+    )
+
+    HttpMethodOverrideMiddleware().process_request(request)
+
+    assert_equal(request.method, 'PUT')
